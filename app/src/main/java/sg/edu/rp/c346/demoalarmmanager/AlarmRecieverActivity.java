0db38c4aeb4db97133491454ebf6166b835b009e
appletree98 <15017452@myrp.edu.sg>
2017-05-26 10:45:38 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoalarmmanager/AlarmRecieverActivity.java
@@ -0,0 +1,19 @@
+package sg.edu.rp.c346.demoalarmmanager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AlarmRecieverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_reciever);
+
+    }
+
+}
